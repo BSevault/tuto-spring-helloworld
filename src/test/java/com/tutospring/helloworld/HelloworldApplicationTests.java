48cb3a743c020bb49b8f7642f11fb475d8d5a497
BSevault <b.sevault@gmail.com>
2023-01-08 21:46:46 +0100

    implemented test

--- src/test/java/com/tutospring/helloworld/HelloworldApplicationTests.java
+++ src/test/java/com/tutospring/helloworld/HelloworldApplicationTests.java
@@ -1,13 +1,32 @@
 package com.tutospring.helloworld;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.tutospring.helloworld.service.BusinessService;
 
 @SpringBootTest
 class HelloworldApplicationTests {
 
+    @Autowired
+    private BusinessService bs;
+
     @Test
     void contextLoads() {
+    }
+
+
+    @Test
+    void testGetHelloWorld(){
+
+        String expected = "Hello World!";
+
+        String result = bs.getHelloWorld().getValue();
+
+        assertEquals(expected, result);
     }
 
 }

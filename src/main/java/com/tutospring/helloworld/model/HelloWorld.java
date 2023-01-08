b19455e2e05ca6ba43177d8907ee219092813892
BSevault <b.sevault@gmail.com>
2023-01-08 21:45:56 +0100

    added getValue method

--- src/main/java/com/tutospring/helloworld/model/HelloWorld.java
+++ src/main/java/com/tutospring/helloworld/model/HelloWorld.java
@@ -3,8 +3,17 @@ package com.tutospring.helloworld.model;
 public class HelloWorld {
     private String value = "Hello World!";
 
+
+
     @Override
     public String toString() {
         return value;
     }
+
+
+
+    public String getValue() {
+        return value;
+    }
+
 }
